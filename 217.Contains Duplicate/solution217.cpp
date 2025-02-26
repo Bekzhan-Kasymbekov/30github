e476032316391e58770377380df51ed8ce1e1a01
bekzhan <kasymbekov_be@auca.kg>
2025-02-26 13:10:19 +0600

    Added hello_world directory

---
+++ 217.Contains Duplicate/solution217.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool containDuplicate(vector<int>& nums) {
+      unordered_set<int> seen;
+      for(int num : nums){
+        if (seen.count(num)){
+          return true;
+        }
+        seen.insert(num);
+      }
+      return false;
+    }
+};
+
+int main() {
+  Solution solution;
+
+  vector<int> nums1 = {1, 2, 3, 4, 5, 1};
+  cout << "Test Case 2: " << (solution.containDuplicate(nums1) ? "True" : "False") << endl;
+
+    return 0;
+  
+}
